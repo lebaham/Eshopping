eb0ca55fb4d732f084c0d846c5bf41e43b180905
lebaham88 <lebaham88@gmail.com>
2018-12-12 23:49:13 +0100

    login

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -8,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   userLoggedin = true;
-  constructor() { }
+  socialAuth: boolean = false; // show Google and FB Sign in only when social auth is enabled
+  error: any;
+  dataLoading: boolean = false;
+  brokenNetwork = false;
+  constructor(public afAuth: AngularFireAuth, private _router: Router, private backendService: FirebaseService) { }
 
   ngOnInit() {
     this.userLoggedin = true;
   }
+
+  ngAfterViewInit(){
+    if(this.afAuth.authState) {
+      this.getAuthStatus();
+    }
+}
+
+  getAuthStatus(){
+    this.backendService.redirectLogin().then(function(result) {
+      if (result.credential) {
+        window.localStorage.setItem("displayName",result.user.displayName);
+        window.localStorage.setItem("email",result.user.email);
+        window.localStorage.setItem("picture",result.user.photoURL);
+        this.isLoggedIn = true;
+      }
+    }).catch(
+      (err) => {
+        this.error = err;
+      })
+  }
+
+  login(loginType, formData?) {
+      this.backendService.login(loginType, formData);
+      /**
+      .then(
+        (success) => {
+          if(formData) {
+            window.localStorage.setItem("email",formData.email);
+          }
+          //console.log(success);
+          this._router.navigate(['/settings']);
+        }).catch(
+        (err) => {
+          this.error = err;
+        })
+      ;
+       */
+    }
 
 }
